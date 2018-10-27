de7be4eb81cdbf7085599f5cc15c8a76f75ff972
polodev <polodev10@gmail.com>
2018-10-28 00:49:03 +0600

    added html file

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/');
+mix.sass('scss/style.scss', 'css/').sourceMaps();
 
 // Full API
 // mix.js(src, output);
